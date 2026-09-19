717d34703011bae19ab76f408503cab24c4088a6
Alize <alizee.sanches@outlook.fr>
2026-09-19 19:15:31 +0200

    ajout gestionnair2 et configuration Xiaomi

--- app/src/main/java/UPsay/decouverteAndroid/MainActivity.java
+++ app/src/main/java/UPsay/decouverteAndroid/MainActivity.java
@@ -2,6 +2,7 @@ package UPsay.decouverteAndroid;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
     public void dessin(View view) {
         TextView editText = (TextView) findViewById(R.id.monTexte);
         editText.setText("YES !!!");
+        setContentView(R.layout.gestionaire2);
     }
 }
